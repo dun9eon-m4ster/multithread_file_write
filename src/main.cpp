1f11090c0bf57223cc95ab7ee8384d16395d5378
egor <e@e.com>
2024-09-19 20:55:36 +0300

    Передача общих данных в конструкторы

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "datagenerator.h"
 #include "filewriter.h"
+#include "threadshareddata.h"
+
+std::shared_ptr<ThreadSharedData> _shared_data(new ThreadSharedData);
 
 int main()
 {
-    DataGenerator generator;
+    DataGenerator generator(_shared_data);
     generator.start();
 
-    FileWriter writer;
+
+    FileWriter writer(_shared_data);
     writer.start();
 
 
