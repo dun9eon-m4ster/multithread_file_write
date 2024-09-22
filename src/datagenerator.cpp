5c3c84a5d3c4feb18fc6f87928e5ad2349462bae
egor <e@e.com>
2024-09-22 18:21:21 +0300

    Массив генерируемых данных;
    изменена релизация метода process()

--- src/datagenerator.cpp
+++ src/datagenerator.cpp
@@ -1,11 +1,21 @@
 #include "datagenerator.h"
 #include <iostream>
 #include "threadshareddata.h"
+#include "data.h"
+#include <cstring>
+
+static const unsigned long long BUFFER_SIZE = 256;
+static_assert (BUFFER_SIZE > 0, "Buffer cannot be zero");
 
 DataGenerator::DataGenerator(std::shared_ptr<ThreadSharedData> _new_shared_data)
     : SharedDataTaskThread(_new_shared_data)
 {
+    for(unsigned long long i = 0; i < BUFFER_SIZE; i++)
+    {
+        buffer.push_back(std::shared_ptr<Data>(new Data));
+    }
 
+    buffer_iterator = buffer.begin();
 }
 
 DataGenerator::~DataGenerator()
@@ -16,14 +26,35 @@ DataGenerator::~DataGenerator()
 
 void DataGenerator::process()
 {
-    std::unique_lock<std::mutex> lk(_data->_mutex);
+    while(state() == Running)
+    {
+        std::unique_lock<std::mutex> lk(_data->_mutex);
 
-    std::cout << "DataGenerator::process()" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    _data->setFlag(ThreadSharedData::PendingData);
+        if(_data->dg_flags.is_generation_started)
+            _data->_cv.wait(lk, [this]{return _data->shared_flags.is_notified;});
+        else
+            _data->dg_flags.is_generation_started = true;
 
-    lk.unlock();
-    _data->_cv.notify_one();
+        std::cout << "posting data..." << std::endl;
 
-    for(;;);
+        _data->shared_flags.is_pending_data = true;
+
+        if(!_data->fw_flags.is_working)
+            _data->shared_flags.is_notified = false;
+
+        memset((*buffer_iterator)->buffer, 0xFF, (*buffer_iterator)->size);
+        _data->_msg_queue.push(*buffer_iterator);
+
+        buffer_iterator++;
+        if(buffer_iterator == buffer.end())
+        {
+            _data->dg_flags.is_generation_finished = true;
+            stop();
+        }
+
+        std::cout << "posting data complete" << std::endl;
+
+        lk.unlock();
+        _data->_cv.notify_one();
+    }
 }
--- src/datagenerator.h
+++ src/datagenerator.h
@@ -2,6 +2,9 @@
 #define DATAGENERATOR_H
 
 #include "taskthread.h"
+#include <vector>
+
+struct Data;
 
 class DataGenerator : public SharedDataTaskThread
 {
@@ -9,6 +12,10 @@ public:
     explicit DataGenerator(std::shared_ptr<ThreadSharedData> _new_shared_data);
     ~DataGenerator();
     virtual void process() override;
+
+private:
+    std::vector<std::shared_ptr<Data>> buffer;
+    std::vector<std::shared_ptr<Data>>::iterator buffer_iterator;
 };
 
 #endif // DATAGENERATOR_H
