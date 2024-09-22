7e2ea7cf2232251811d973e3ee03a1a94d414a5d
egor <e@e.com>
2024-09-22 12:06:33 +0300

    Класс для подсчёта времени исполнения программы

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include "datagenerator.h"
 #include "filewriter.h"
 #include "threadshareddata.h"
+#include <iostream>
 
 std::shared_ptr<ThreadSharedData> _shared_data(new ThreadSharedData);
+
+class Timer
+{
+public:
+    Timer()
+    {
+        start = std::chrono::steady_clock::now();
+    }
+
+    ~Timer()
+    {
+        auto end = std::chrono::steady_clock::now();
+
+        std::cout << "elapsed time: " << std::chrono::duration<double>(end - start).count() << " seconds" << std::endl;
+    }
+
+private:
+    std::chrono::time_point<std::chrono::steady_clock> start;
+};
+
+Timer t;
 
 int main()
 {
